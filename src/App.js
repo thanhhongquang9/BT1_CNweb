0a677829b7fb78813eaf4f262836be3cecb574e8
HONG QUANG THANH <4501104213@student.hcmue.edu.vn>
2021-10-20 21:18:51 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <h1>Todo List</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
